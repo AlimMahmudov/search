53cf5dc8a372176d0f5c99a43e260a55b763c2ee
Alim Mahmudov <alimmahmudov27@gmail.com>
2025-01-06 22:09:26 +0600

    box

--- src/appProject/homeSections/home/Home.tsx
+++ src/appProject/homeSections/home/Home.tsx
@@ -2,6 +2,7 @@
 import React from "react";
 import { useData } from "../data/data";
 import { useSearch } from "@/stores/SearchStore";
+import scss from "./Home.module.scss";
 
 const Home = () => {
   const { data } = useData();
@@ -11,17 +12,21 @@ const Home = () => {
     el.title.toLowerCase().includes(searchQuery.toLowerCase())
   );
   return (
-    <div>
-      {filteredData.length > 0 ? (
-        filteredData.map((el, index) => (
-          <div className="card" key={index}>
-            <img src={el.image} alt="" />
-            <h1>{el.title}</h1>
-          </div>
-        ))
-      ) : (
-        <p className="no-product">Нет продукта</p>
-      )}
+    <div id={scss.Home}>
+      <div className="container">
+        <div className={scss.home}>
+          {filteredData.length > 0 ? (
+            filteredData.map((el, index) => (
+              <div className={scss.box} key={index}>
+                <img src={el.image} alt="" />
+                <h1>{el.title}</h1>
+              </div>
+            ))
+          ) : (
+            <p className="no-product">Нет продукта</p>
+          )}
+        </div>
+      </div>
     </div>
   );
 };
