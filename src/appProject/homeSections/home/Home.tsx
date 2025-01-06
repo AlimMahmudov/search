9dc16483e87c1b8f94dd3516f8dc4f3d4a4a97c6
Alim Mahmudov <alimmahmudov27@gmail.com>
2025-01-06 22:30:24 +0600

    mk title

--- src/appProject/homeSections/home/Home.tsx
+++ src/appProject/homeSections/home/Home.tsx
@@ -15,16 +15,19 @@ const Home = () => {
     <div id={scss.Home}>
       <div className="container">
         <div className={scss.home}>
-          {filteredData.length > 0 ? (
-            filteredData.map((el, index) => (
-              <div className={scss.box} key={index}>
-                <img src={el.image} alt="" />
-                <h1>{el.title}</h1>
-              </div>
-            ))
-          ) : (
-            <p className="no-product">Нет продукта</p>
-          )}
+          <h1>Мортол комбат персонажи</h1>
+          <div className={scss.block}>
+            {filteredData.length > 0 ? (
+              filteredData.map((el, index) => (
+                <div className={scss.box} key={index}>
+                  <img src={el.image} alt="" />
+                  <h1>{el.title}</h1>
+                </div>
+              ))
+            ) : (
+              <p className="no-product">Нет продукта</p>
+            )}
+          </div>
         </div>
       </div>
     </div>
